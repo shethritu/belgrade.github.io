eb5999394052b0222c1fd72eefdde161a9b0bafa
shethritu <96720702+shethritu@users.noreply.github.com>
2024-09-06 19:50:13 +0530

    Add files via upload

---
+++ js/custom_js.js
@@ -0,0 +1,9 @@
+$('.slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots: true
+});
+$(".section-height").matchHeight();
+$(".title_height").matchHeight();
